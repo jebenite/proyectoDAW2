d89f06f4a5a0a0ac1a6bf1d2688b26147cbcfaed
Brannybk <brajchit@espol.edu.ec>
2016-09-06 03:04:26 -0500

    actualizado rutas y layouts handlebars del modulo /operario

--- mean-proy3/routes/operario.js
+++ mean-proy3/routes/operario.js
@@ -2,30 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 //operario
-router.get('/', function(req, res, next) {
+router.get('/pacientes', function(req, res, next) {
     res.render('operarios/pacientes', {
-        title: 'Pacientes', layout: false
+        title: 'Pacientes'
     });
 });
-router.get('/paciente-crear.ejs', function(req, res, next) {
+router.get('/paciente-crear', function(req, res, next) {
     res.render('operarios/paciente-crear', {
-        title: 'Crear Paciente', layout: false
+        title: 'Crear Paciente'
     });
 });
-router.get('/muestras-index.ejs', function(req, res, next) {
+router.get('/muestras-index', function(req, res, next) {
     res.render('operarios/muestras-index', {
-        title: 'Muestras', layout: false
+        title: 'Muestras'
     });
 });
-router.get('/muestra-crear.ejs', function(req, res, next) {
+router.get('/muestra-crear', function(req, res, next) {
     res.render('operarios/muestra-crear', {
-        title: 'Crear Muestra', layout: false
+        title: 'Crear Muestra'
     });
 });
 
 router.get('/estadisticas', function(req, res, next) {
     res.render('operarios/estadisticas', {
-        title: 'Estadisticas', layout: false
+        title: 'Estadisticas'
     });
 });
 
